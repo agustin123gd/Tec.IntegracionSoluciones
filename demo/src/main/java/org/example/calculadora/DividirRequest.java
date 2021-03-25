2a0d8e66a3c8eb4c18891a5e3b0d83dadd055288
agustin123gd <71846715+agustin123gd@users.noreply.github.com>
2021-03-24 19:43:15 -0600

    Add files via upload

---
+++ demo/src/main/java/org/example/calculadora/DividirRequest.java
@@ -0,0 +1,69 @@
+package org.example.calculadora;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "a",
+    "b"
+})
+@XmlRootElement(name = "DividirRequest")
+
+public class DividirRequest {
+    @XmlElement(required = true)
+    protected String a;
+    @XmlElement(required = true)
+    protected String b;
+
+    /**
+     * Obtiene el valor de la propiedad a.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getA() {
+        return a;
+    }
+
+    /**
+     * Define el valor de la propiedad a.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setA(String value) {
+        this.a = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad b.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getB() {
+        return b;
+    }
+
+    /**
+     * Define el valor de la propiedad b.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setB(String value) {
+        this.b = value;
+    }
+}
